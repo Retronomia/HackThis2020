20995e0f9f72cd5b86e638539b865057289765bd
dont-Blink <rosen10@purdue.edu>
2020-08-08 12:18:53 -0700

    not working sad face

--- add_class.js
+++ add_class.js
@@ -1,15 +1,32 @@
 function clickHandler() {
-    let div = document.createElement("div");
-    div.setAttribute("id", "section");
-    
+    /*<div class='section'>
+            <h1>DOE</h1>
+            <div id='imgdiv'><img src="thonq.png"></img></div>
+            <p>A deer, a female deer</p>
+        </div>
+    */
 
+    
+    let div = document.createElement("div");
+    //div.class = "section";
+    //div.id = "Class";
+    //div.onclick = "location.href='classpage.html?class='" + this.id;
+    div.setAttribute("class", "section");
+    div.setAttribute("id", "Class");
+    div.setAttribute("onclick", "location.href='classpage.html?class=this.id;");
+    div.setAttribute("onclick", "location.href='home.html';");
+    div.setAttribute("style", "cursor: pointer;");
+    
+    let headdiv = document.createElement("div");
+    headdiv.setAttribute("style", "pointer-events: none;");
     let header = document.createElement("h1");
-    header.appendChild(document.createTextNode("Class name"));
+    header.appendChild(document.createTextNode("Class"));
     header.setAttribute("contenteditable", "true");
-    div.appendChild(header);
+    headdiv.appendChild(header);
+    div.appendChild(headdiv);
 
     let imgdiv = document.createElement("div");
-    imgdiv.setAttribute("id", "imgdiv");
+    imgdiv.setAttribute("class", "imgdiv");
     let img = document.createElement("img");
     img.src = "thonq.png";
     imgdiv.appendChild(img);
@@ -21,5 +38,4 @@ function clickHandler() {
 
     document.getElementById('classes').appendChild(div);  
 
-    //document.body.appendChild(div);   
 }
---
+++ components/student_class.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#classes');
+ReactDOM.render(e(LikeButton), domContainer);
+
+/*
+<script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>>
+<script src="components/student_class.js"></script>
+*/
